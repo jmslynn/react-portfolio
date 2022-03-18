d7780bf83553e3add36b3d95152829a5f92f1ed0
jmslynn <72352820+jmslynn@users.noreply.github.com>
2022-03-17 21:20:14 -0400

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+
+export const skills = [
+  "ReactJS", "JavaScript", "CSS", "HTML", "Bootstrap", "tailwindcss", "Other Skills: MySQL, Java"
+]
+export const projects = [
+  {
+    title: "Budget Squad",
+    subtitle: "React, JavaScript, BootStrap, Jira",
+    description:
+      "A group project to create a budgeting application. I'm currently creating a budget component to display the amount spent in each budget category with a progress bar.",
+    image: "category-row-component.PNG",
+    link: "n/a",
+  },
+  {
+    title: "RuralNet",
+    subtitle: "Svelte, Tailwind",
+    description:
+      "A Code for the Upper Valley, Code for America Brigade project.",
+    image: "ruralNetDraft.png",
+    link: "https://ruralnetv2.herokuapp.com",
+  },
+  {
+    title: "FoodResources",
+    subtitle: "Tailwind CSS, HTML, UI/UX",
+    description:
+    "A web-based app to help Upper Valley residents locate food resources. A Code for the Upper Valley, Code for America Brigade project.",
+    image: "foodResourcesMobile.png",
+    link: "https://food-resources.herokuapp.com",
+  },
+  {
+    title: "Northern Stage",
+    subtitle: "Joomla!, HTML, CSS, PHP",
+    description:
+      "Redesigned website for regional theater company.",
+    image: "finaldesign2.jpg",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+// food resources designs: https://www.canva.com/design/DAEVks6bblM/F1_ZRFENRVg6TofCw3o7nQ/view#1
