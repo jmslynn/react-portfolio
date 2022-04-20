02035fec7372e82f07c9d6a1f9aa98b9c670b81a
jmslynn <72352820+jmslynn@users.noreply.github.com>
2022-04-19 20:28:17 -0400

    add gitignore, add colors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="text-lt-blue-shadow bg-dark-blue body-font">
       <Navbar />
       <About />
       <Projects />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
+    <header className="bg-lighter-blue text-lt-blue-shadow md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <nav className="title-font font-medium text-white mb-4 md:mb-0">
+        <nav className="title-font font-medium text-lt-blue-shadow mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Jennifer S. Lynn
           </a>
