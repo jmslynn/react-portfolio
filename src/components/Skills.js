1e340c04217eb14a56bf57bdb8ccad007142aba4
jmslynn <72352820+jmslynn@users.noreply.github.com>
2022-04-27 15:25:31 -0400

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,9 +12,7 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            I recently completed a full-stack software developer bootcamp where I updated my HTML, CSS, JavaScript and MySQL skills while also learning Java, ReactJS, JQUERY, and Spring Boot. I also learned how to create and work with APIs. 
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
